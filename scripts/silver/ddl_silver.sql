aeb3b920f1cb6ea063e405d8ad9fb91650d6b12b
yugulsharma899 <yugulsharma899@gmail.com>
2026-06-16 20:48:01 +0530

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,87 @@
+
+-- DDL Scripts for silver to create Tables
+
+if OBJECT_ID ('silver.crm_cust_info' , 'U') is not null
+	drop table silver.crm_cust_info;
+go
+
+create table silver.crm_cust_info(
+	cst_id int,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_material_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date date,
+	dwh_create_date datetime2 default getdate()
+);
+go
+
+if OBJECT_ID ('silver.crm_prd_info' , 'U') is not null
+	drop table silver.crm_prd_info;
+create table silver.crm_prd_info(
+	prd_id int,
+	cat_id nvarchar(50),
+	prd_key nvarchar(50),
+	prd_nm nvarchar(50),
+	prd_cost int,
+	prd_line nvarchar(50),
+	prd_start_dt date,
+	prd_end_dt date,
+	dwh_create_date datetime2 default getdate()
+);
+go
+
+if OBJECT_ID ('silver.crm_sales_details' , 'U') is not null
+	drop table silver.crm_sales_details;
+go
+
+create table silver.crm_sales_details(
+	sls_ord_num nvarchar(50),
+	sls_prd_key nvarchar(50),
+	sls_cust_id int,
+	sls_order_dt date,
+	sls_ship_dt date,
+	sls_due_dt date,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int,
+	dwh_create_date datetime2 default getdate()
+);
+go
+
+if OBJECT_ID ('silver.erp_cust_ax12' , 'U') is not null
+	drop table silver.erp_cust_ax12;
+go
+
+create table silver.erp_cust_ax12(
+	cid		nvarchar(50),
+	bdate	date,
+	gen		nvarchar(50),
+	dwh_create_date datetime2 default getdate()
+);
+go
+
+if OBJECT_ID ('silver.erp_loc_a101' , 'U') is not null
+	drop table silver.erp_loc_a101;
+go
+
+create table silver.erp_loc_a101(
+	cid		nvarchar(50),
+	cntry	nvarchar(50),
+	dwh_create_date datetime2 default getdate()
+);
+go
+
+if OBJECT_ID ('silver.erp_px_cat_g1v2' , 'U') is not null
+	drop table silver.erp_px_cat_g1v2;
+go
+
+create table silver.erp_px_cat_g1v2(
+	id		nvarchar(50),
+	cat		nvarchar(50),
+	subcat	nvarchar(50),
+	maintanence	nvarchar(50),
+	dwh_create_date datetime2 default getdate()
+);
+go
